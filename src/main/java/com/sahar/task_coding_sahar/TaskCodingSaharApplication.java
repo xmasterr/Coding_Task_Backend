7fb553d8755b80c2f369e034fd150dc04c017339
xmasterr <33792788+xmasterr@users.noreply.github.com>
2021-09-17 09:03:23 +0200

    First Commit

---
+++ src/main/java/com/sahar/task_coding_sahar/TaskCodingSaharApplication.java
@@ -0,0 +1,14 @@
+package com.sahar.task_coding_sahar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCodingSaharApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TaskCodingSaharApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sahar/task_coding_sahar/TaskCodingSaharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahar.task_coding_sahar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskCodingSaharApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
